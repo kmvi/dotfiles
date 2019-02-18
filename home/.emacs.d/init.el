c1ec5b45e45d1c29adfe252d12c8bde736a1063a
kmvi <kmvi@users.noreply.github.com>
2019-02-18 11:48:10 +0300

    emacs initial config

---
+++ home/.emacs.d/init.el
@@ -0,0 +1,47 @@
+(defun system-is-linux()
+  (string-equal system-type "gnu/linux"))
+(defun system-is-windows()
+  (string-equal system-type "windows-nt"))
+
+(when (system-is-windows)
+  (set-face-attribute 'default nil :font "PragmataPro-12"))
+
+(setq inhibit-startup-message t) ;; don't show startup message
+(setq inhibit-splash-screen t) ;; and spalsh screen
+(setq default-tab-width 4) ;; tab size
+(global-hl-line-mode 1) ;; highlight active line
+
+(setq make-backup-files nil) ;; disable backups
+
+;; dired-mode
+(require 'dired)
+(setq dired-recursive-delete 'top)
+
+;; syntax highlighting
+(require 'font-lock)
+(global-font-lock-mode t)
+(setq font-lock-maximum-decoration t)
+
+(show-paren-mode t)
+(delete-selection-mode t)
+
+(set-language-environment 'UTF-8)
+
+;; display the name of the current buffer in the title bar
+(setq frame-title-format "emacs: %b")
+
+;; org-mode
+(require 'org)
+
+;; line numbering
+(require 'linum)
+(line-number-mode t)
+(global-linum-mode t)
+(column-number-mode t)
+(setq linum-format " %d")
+
+;; window size
+(when (window-system)
+  (set-frame-size (selected-frame) 100 30))
+
+
